0256208ae095fb56665c53031628ff78c6508992
ganesh129 <gksfromgaya@gmail.com>
2023-07-31 23:13:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+
+const app=()=>{
+    <>
+    <h1>My Youtube</h1>
+    </>
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<app/>,document.getElementById("root"));
